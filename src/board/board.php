b21385374670581bc33c22f6bdb538e8588fba2f
JIN-CHEOL <35950321+JIN-CHEOL@users.noreply.github.com>
2018-02-01 21:36:29 +0900

    board.php 개발 완료 - 진철

--- src/board/board.php
+++ src/board/board.php
@@ -31,8 +31,35 @@ $param = $controller->param;
     function insertBoard(){
         location.href='/board/board_reg.php';
     }
+    function searchBoard(){
+        var search_col = $('#search_col').val();
+        var search_text = $('#search_text').val();
+        var form = new CommonForm('commonForm');
+        form.addParam('search_col',search_col);
+        form.addParam('search_text',search_text);
+        form.setUrl('/board/board.php');
+        form.submit();
+    }
+    function movePage(page){
+        var search_col = $('#search_col').val();
+        var search_text = $('#search_text').val();
+        var form = new CommonForm('commonForm');
+        form.addParam('search_col',search_col);
+        form.addParam('search_text',search_text);
+        form.addParam('currPage',page);
+        form.setUrl('/board/board.php');
+        form.submit();
+    }
 </script>
 <div id="center">
+    <div>
+        <select name="search_col" id="search_col">
+            <option value="title">제목</option>
+            <option value="writer">작성자</option>
+        </select>
+        <input type="text" name="search_text" id="search_text" value="<?php echo $controller->param->post('search_text')?>">
+        <button type="button" onclick="searchBoard()">조회</button>
+    </div>
     <table>
         <tr>
             <th>No.</th>
@@ -46,7 +73,7 @@ $param = $controller->param;
             if($count>0){
                 while($row = $controller->db->result->fetch_assoc()){
                     echo "<tr>
-                                <td>".$row['F_IDX']."</td>
+                                <td>".$row['F_ROWNUM']."</td>
                                 <td><a href='#' onclick=javascript:viewDetail('".$row['F_IDX']."')>".$row['F_TITLE']."</a></td>
                                 <td>".$row['F_WRITER']."</td>
                                 <td>".$row['F_HIT']."</td>
@@ -59,6 +86,27 @@ $param = $controller->param;
             $controller->db->DBOut();
         ?>
     </table>
+    <div class="pageNavigation">
+        <?php
+        $page = floor($controller->currPage / $controller->pageSize) * $controller->pageSize +1;
+        if($controller->currPage > $controller->pageSize)
+            echo "<a href='#' onclick='movePage(".($page-10).");'>이전</a>";
+
+        $i = $page;
+        if(floor($controller->totalRow / $controller->pageSize) < $controller->countPage){
+            $controller->countPage = floor($controller->totalRow / $controller->pageSize)+1;
+        }
+        while($i<=$controller->countPage){
+            echo "<a href='#' onclick=movePage(".$i.");>".$i."</a>";
+          $i=$i+1;
+       }
+       $controller->countPage=10;
+        if($controller->currPage > $controller->totalRow-floor($controller->totalRow / $controller->countPage))
+            echo "<a href='#' onclick='movePage(".($page+10).");'>다음</a>";
+
+        //echo "<br> currPage:".$controller->currPage."// totalRow:".$controller->totalRow."//countPage:".$controller->countPage."//firstRow:".$controller->firstRow;
+        ?>
+    </div>
     <div><button type="button" onclick="insertBoard();">글등록</button></div>
 </div>
 
--- src/board/boardController.php
+++ src/board/boardController.php
@@ -15,20 +15,56 @@ class boardController
 {
     public $param;
     public $db;
+    public $currPage;
+    public $totalRow;
+    public $firstRow;
+    public $pageSize;
+    public $countPage;
     public function boardController(){
         $this->param = new param;
     }
     public function showBoard(){
         $db = new DB;
         $db->DBConn();
-        $db->query = "SELECT 
-                        F_IDX,
-                        F_TITLE,
-                        F_CONTENT,
-                        F_WRITER,
-                        F_HIT,
-                        F_WRITE_DATE
-                        FROM t_board";
+        $searchQuery = "";
+        if($this->param->post('search_col') != null && $this->param->post('search_text') != null){
+            $searchCol = "";
+            if($this->param->post('search_col') =='title'){
+                $searchCol = "F_TITLE";
+            }else if($this->param->post('search_col') =='writer'){
+                $searchCol = "F_WRITER";
+            }
+            $searchQuery = "AND ".$searchCol." = '".$this->param->post('search_text')."'";
+        }
+
+        $this->currPage = $this->param->post('currPage');
+        if($this->currPage == null)$this->currPage = 1;
+
+        $this->pageSize = 10;
+        $this->countPage = 10;
+        $this->firstRow = ($this->currPage*$this->pageSize)-$this->pageSize+1;
+
+        $db->query = "SELECT COUNT(*) AS F_CNT FROM T_BOARD WHERE 1=1 ".$searchQuery;
+        $db->DBQuery();
+        $data = $db->result->fetch_assoc();
+        $this->totalRow = $data['F_CNT'];
+//        $this->totalRow = 20;
+
+        $db->query = "SELECT * FROM(
+                        SELECT 
+                        @ROWNUM:=@ROWNUM + 1 AS F_ROWNUM,
+                        T_BOARD.F_IDX,
+                        T_BOARD.F_TITLE,
+                        T_BOARD.F_CONTENT,
+                        T_BOARD.F_WRITER,
+                        T_BOARD.F_HIT,
+                        T_BOARD.F_WRITE_DATE
+                        FROM T_BOARD,(SELECT @ROWNUM:=0)R
+                        ORDER BY T_BOARD.F_IDX DESC
+                      )T
+                      WHERE 1=1
+                      ".$searchQuery."
+                      LIMIT ".($this->firstRow-1).",".$this->pageSize;
         $db->DBQuery();
         if($db->result){
             $this->db = $db;
